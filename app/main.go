7429d9023db5d7c78f158abc44c3e11c8e70d695
Nuno Tomé <nunotomeit@gmail.com>
2024-07-18 03:59:48 +0100

    done

--- app/main.go
+++ app/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 var SYS_COMFIG_LINK_LIST = []string{}
 var SYS_COMFIG_DESTINATION_FOLDER string
+var SYS_COMFIG_DEBUG_MODE bool = true
 
 // download
 func download(link2Download string) {
@@ -38,6 +42,50 @@ func loadLinksFromFile(filePath string) ([]string, error) {
 
 	return links, nil
 }
+
+// downloadFile faz o download do conteúdo de um link e salva-o num ficheiro local
+func downloadFile(url, folderPath string) error {
+	// Obter o nome do ficheiro a partir da URL
+	parts := strings.Split(url, "/")
+	fileName := parts[len(parts)-1]
+	filePath := filepath.Join(folderPath, fileName)
+
+	// Fazer o pedido HTTP
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("erro ao fazer o pedido HTTP: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Criar o ficheiro local
+	outFile, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("erro ao criar o ficheiro: %v", err)
+	}
+	defer outFile.Close()
+
+	// Copiar o conteúdo da resposta HTTP para o ficheiro local
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return fmt.Errorf("erro ao copiar o conteúdo: %v", err)
+	}
+
+	return nil
+}
+
+// downloadLinks faz o download de todos os links para uma pasta especificada
+func downloadLinks(links []string, folderPath string) error {
+	for _, link := range links {
+		fmt.Printf("A fazer download de %s...\n", link)
+		err := downloadFile(link, folderPath)
+		if err != nil {
+			fmt.Printf("Erro ao fazer download de %s: %v\n", link, err)
+		} else {
+			fmt.Printf("Download de %s concluído.\n", link)
+		}
+	}
+	return nil
+}
 func main() {
 
 	//var linkListFile string = ""
@@ -45,9 +93,11 @@ func main() {
 	var linkListFileFolder string = "C:\\Users\\nunot\\Documents\\_Documentos\\Projectos\\mass link downloder\\repositorio\\app"
 	linkListFile := linkListFileFolder + "\\" + linkListFileName
 
+	var downloadFolder string = "C:\\Users\\nunot\\Documents\\_Documentos\\Projectos\\mass link downloder\\repositorio\\app\\files"
+
 	//config file defenition
 	/*var configFileFileName string = "configFileFileName"
-	var configFileFileFolder string = "C:\\Users\\nunot\\Documents\\_Documentos\\Projectos\\mass link downloder\\repositorio\\app"
+	var configFileFileFolder string = "C:\\Users\\nunot\\Documents\\_Documentos\\Projectos\\mass link downloder\\repositorio\\app\\files"
 	configFile := configFileFileName + "\\" + configFileFileFolder
 	*/
 
@@ -57,10 +107,25 @@ func main() {
 		return
 	}
 
+	//#DEBUG
 	fmt.Println("Loaded links:")
 	for _, link := range links {
 		fmt.Println(link)
 	}
+
+	// Criar a pasta de downloads se não existir
+	if _, err := os.Stat(downloadFolder); os.IsNotExist(err) {
+		os.Mkdir(downloadFolder, 0755)
+	}
+
+	// Fazer o download de todos os links
+	err = downloadLinks(links, downloadFolder)
+	if err != nil {
+		fmt.Printf("Erro ao fazer o download dos links: %v\n", err)
+	} else {
+		fmt.Println("Todos os downloads foram concluídos.")
+	}
+
 }
 
 /*
