c474497b7c0933044bd913cf922ef5a0d2cdc6aa
Nuno Tomé <nunotomeit@gmail.com>
2024-07-18 06:08:56 +0100

    code cleaning

--- app/main.go
+++ app/main.go
@@ -134,30 +134,3 @@ func main() {
 	}
 
 }
-
-/*
-func main() {
-
-	//var linkListFile string = ""
-	var linkListFileName string = "linkListFileName.txt"
-	var linkListFileFolder string = "C:\\Users\\nunot\\Documents\\_Documentos\\Projectos\\mass link downloder\\repositorio\\app"
-	linkListFile := linkListFileFolder + "\\" + linkListFileFolder
-
-	//var configFile string = ""
-	var configFileFileName string = "configFileFileName"
-	var configFileFileFolder string = "C:\\Users\\nunot\\Documents\\_Documentos\\Projectos\\mass link downloder\\repositorio\\app"
-	configFile := configFileFileName + "\\" + configFileFileFolder
-
-	fmt.Println(linkListFile)
-	fmt.Println(linkListFileName)
-	fmt.Println(linkListFileFolder)
-	fmt.Println(linkList)
-
-	fmt.Println(configFile)
-	fmt.Println(configFileFileName)
-	fmt.Println(configFileFileFolder)
-
-	load_link_list_from_file(linkListFile)
-
-}
-*/
