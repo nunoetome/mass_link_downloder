59f2e1421de3d27f249d51f84e2500f2cd6231e9
Nuno Tomé <nunotomeit@gmail.com>
2024-07-18 03:34:13 +0100

    added func loadLinksFromFile(filePath string) ([]string, error)

--- app/main.go
+++ app/main.go
@@ -1,23 +1,69 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
-var linkList = []string{}
-var config1 = []string{}
+var SYS_COMFIG_LINK_LIST = []string{}
+var SYS_COMFIG_DESTINATION_FOLDER string
 
-func download() {
-
-}
-
-func load_link_list_from_file() {
+// download
+func download(link2Download string) {
 
 }
 
-func lllff() {
+// loadLinksFromFile lê um ficheiro de texto que contém uma lista de links e retorna uma slice de strings com esses links
+func loadLinksFromFile(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao abrir o ficheiro: %v", err)
+	}
+	defer file.Close()
 
+	var links []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" {
+			links = append(links, line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("erro ao ler o ficheiro: %v", err)
+	}
+
+	return links, nil
 }
+func main() {
+
+	//var linkListFile string = ""
+	var linkListFileName string = "linkListFileName.txt"
+	var linkListFileFolder string = "C:\\Users\\nunot\\Documents\\_Documentos\\Projectos\\mass link downloder\\repositorio\\app"
+	linkListFile := linkListFileFolder + "\\" + linkListFileName
+
+	//config file defenition
+	/*var configFileFileName string = "configFileFileName"
+	var configFileFileFolder string = "C:\\Users\\nunot\\Documents\\_Documentos\\Projectos\\mass link downloder\\repositorio\\app"
+	configFile := configFileFileName + "\\" + configFileFileFolder
+	*/
+
+	links, err := loadLinksFromFile(linkListFile)
+	if err != nil {
+		fmt.Printf("Erro: %v\n", err)
+		return
+	}
+
+	fmt.Println("Loaded links:")
+	for _, link := range links {
+		fmt.Println(link)
+	}
+}
+
+/*
 func main() {
 
 	//var linkListFile string = ""
@@ -39,4 +85,7 @@ func main() {
 	fmt.Println(configFileFileName)
 	fmt.Println(configFileFileFolder)
 
+	load_link_list_from_file(linkListFile)
+
 }
+*/
